2b3cf1f4ede30d1db6c968a416b8a68809242090
Vishal Haridas <vishalharidasofficial@gmail.com>
2026-04-06 16:59:37 +0400

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs) {
+        let sortedS = strs.map(s => s.split('').sort().join(''))
+        let map = new Map()
+
+        for (let [i, s] of sortedS.entries()){
+            if (!map[s]){
+                map[s] = []
+            }
+            map[s].push(strs[i])
+        }
+
+        return Object.values(map)
+    }
+}
